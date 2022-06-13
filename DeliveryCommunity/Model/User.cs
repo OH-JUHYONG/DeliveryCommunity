ecf5ae03d2e3c2e38cc92eb565ae348edbc68586
comment2 <86146642+comment2@users.noreply.github.com>
2022-06-13 22:59:21 +0900

    유저정보추가

---
+++ DeliveryCommunity/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryCommunity.Model
+{
+    public class User
+    {
+        public static string Place { get; set; }
+        public static string Name { get; set; }
+    }
+}
