8e02b1fe2c284a2ea9b1988d10cd75f12d5b656a
comment2 <86146642+comment2@users.noreply.github.com>
2022-06-14 17:00:24 +0900

    가라 로그인 완료

---
+++ DeliveryCommunity/ViewModel/LoginVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeliveryCommunity.ViewModel.Commands;
+
+namespace DeliveryCommunity.ViewModel
+{
+    public class LoginVM// 주의: 서버가 없으므로 인증같은거 없음
+    {
+        public LoginButtonCommand LoginButtonCommand { get; set; }
+        public LoginVM()
+        {
+            LoginButtonCommand = new LoginButtonCommand(this);
+        }
+
+    }
+}
